8c8775b1bb09aea69453bbb047a4fc0bf99b8b77
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-03-18 15:19:23 +0000

    adding loading bar

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const output = document.querySelector("#joke");
 const gif_api_key = "Mvtk1VOaZhdRLdIqKzWqgXQ0gnMl894B";
 const image = document.querySelector("#gif_image");
 const output1 = document.querySelector("#punchline");
-const joke = document.querySelector("joke_button")
+const loader = document.querySelector("#loader");
+
+// Gif button 
 
 document.querySelector("#search_button").addEventListener("click", (event) => {
 
@@ -16,6 +18,9 @@ document.querySelector("#search_button").addEventListener("click", (event) => {
 
 
     fetch(`https://api.giphy.com/v1/gifs/random?api_key=${gif_api_key}&tag=${name}&limit=1`)
+
+        .then(document.getElementById("loader").className = "loader-view")
+
         .then((response) => {
             if (!response.ok) throw new Error(response.status);
             return response.json();
@@ -23,13 +28,26 @@ document.querySelector("#search_button").addEventListener("click", (event) => {
         .then((data) => {
             image.src = data.data.image_original_url;
             image.alt = data.data.title;
+            document.getElementById("loader").className = "loader";
         })
+        .catch((error) => {
+            console.log(error);
+            if (error.message === "404") {
+                output.textContent = `⚠️ Couldn't find ${name}`;
+            } else {
+                output.textContent = "⚠️ Something went wrong";
+            }
+        });
+});
 
-    .then(() => fetch('https://official-joke-api.appspot.com/jokes/ten')
+
+document.querySelector("#joke_button").addEventListener("click", (event) => {
+
+    fetch('https://official-joke-api.appspot.com/jokes/ten')
             .then((response) => {
                 if (!response.ok) throw new Error(response.status);
                 return response.json();
-            }))
+            })
         .then((object) => {
             output.textContent = object[0].setup;
             var punch = object[0].punchline;
@@ -45,4 +63,4 @@ document.querySelector("#search_button").addEventListener("click", (event) => {
                 output.textContent = "⚠️ Something went wrong";
             }
         });
-});
+})
