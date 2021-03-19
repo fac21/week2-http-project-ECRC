c555664813bd62ea550072770168a874d2247a22
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-03-19 12:41:24 +0000

    changing variables names

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const form = document.querySelector("form");
-const output = document.querySelector("#joke");
+const joke = document.querySelector("#joke");
 const gif_api_key = "Mvtk1VOaZhdRLdIqKzWqgXQ0gnMl894B";
 const image = document.querySelector("#gif_image");
-const output1 = document.querySelector("#punchline");
+const punchline = document.querySelector("#punchline");
 const loader = document.querySelector("#loader");
 
 // Gif button 
@@ -11,8 +11,8 @@ document.querySelector("#search_button").addEventListener("click", (event) => {
 
     event.preventDefault();
 
-    output.innerHTML = "";
-    output1.innerHTML= "";
+    joke.innerHTML = "";
+    punchline.innerHTML= "";
 
     const formData = new FormData(form);
     const name = formData.get("gif_name");
@@ -34,9 +34,9 @@ document.querySelector("#search_button").addEventListener("click", (event) => {
         .catch((error) => {
             console.log(error);
             if (error.message === "404") {
-                output.textContent = `⚠️ Couldn't find ${name}`;
+                joke.textContent = `⚠️ Couldn't find ${name}`;
             } else {
-                output.textContent = "⚠️ Something went wrong";
+                joke.textContent = "⚠️ Something went wrong";
             }
         });
 
@@ -54,20 +54,20 @@ document.querySelector("#joke_button").addEventListener("click", (event) => {
                 return response.json();
             })
         .then((object) => {
-            output1.textContent = "";
-            output.textContent = object[0].setup;
+            punchline.textContent = "";
+            joke.textContent = object[0].setup;
             var punch = object[0].punchline;
             let interval = setInterval(function() {
                 clearInterval(interval);
-                output1.textContent = punch;
+                punchline.textContent = punch;
             }, 1500)
         })
         .catch((error) => {
             console.log(error);
             if (error.message === "404") {
-                output.textContent = `⚠️ Couldn't find ${name}`;
+                joke.textContent = `⚠️ Couldn't find ${name}`;
             } else {
-                output.textContent = "⚠️ Something went wrong";
+                joke.textContent = "⚠️ Something went wrong";
             }
         });
 })
